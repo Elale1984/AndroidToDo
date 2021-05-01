a8a38d6eb6190c15384cd91173aefdfb7a1ccce5
elale1984 <edward.gratefuldead1@gmail.com>
2021-05-01 00:13:44 -0500

    added FAB function to call new task activity. Created new task activity.

--- app/src/main/java/ez/todo/Event.java
+++ app/src/main/java/ez/todo/Event.java
@@ -1,4 +1,4 @@
-import java.util.Date;
+package ez.todo;
 
 public class Event {
 
--- app/src/main/java/ez/todo/MainActivity.java
+++ app/src/main/java/ez/todo/MainActivity.java
@@ -4,21 +4,35 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton fabAdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fabAdd = findViewById(R.id.fab_addEvent);
 
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddEvent.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
